c43b5b48e7b66f4d9a55357b11130aa49847d938
Cookieizi <42553582+Cooki3izi@users.noreply.github.com>
2021-01-31 00:49:10 +0300

    Add files via upload

---
+++ APIs.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using RestSharp;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace eveSwapper
+{
+
+    class APIs
+    {
+        public RestClient client = new RestClient();
+        public RestRequest request = new RestRequest();
+        public CookieContainer cookies = new CookieContainer();
+        static private Random random = new Random();
+        public string API_BASE = "https://i.instagram.com/api/v1";
+        string CONSENT_PATH = "/consent/update_dob/";
+        string CURRENT_PATH = "/accounts/current_user/?edit=true";
+        string SET_PATH = "/accounts/set_username/";
+        string BIO_PATH = "/accounts/set_biography/";
+        string WEB_BASE = "https://www.instagram.com";
+        string CHECK14_PATH = "/accounts/web_create_ajax/attempt/";
+        public string USER_AGENT = $"Instagram {random.Next(5, 100)}.{random.Next(6, 10)}.{random.Next(0, 10)} Android (18/2.1; 160dpi; 720x900; ZTE; LAVA-9L7EZ; pdfz; hq3143; en_US)";
+        public string username;
+        public string email;
+        public string phone_number;
+        public string biography;
+
+        public HttpStatusCode Consent(string Cookies)
+        {
+            client = new RestClient(API_BASE);
+            client.Proxy = null;
+            request = new RestRequest(CONSENT_PATH, Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("X-CSRFToken", "missing");
+            headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            headers.Add("Connection", "keep-alive");
+            request.AddCookie("sessionid",Cookies);
+            client.UserAgent = USER_AGENT;
+            client.AddDefaultHeaders(headers);
+            request.AddParameter("signed_body", "SIGNATURE.{\"current_screen_key\":\"dob\",\"day\":\"1\",\"year\":\"1998\",\"month\":\"1\"}", ParameterType.RequestBody);
+            var execute = client.Execute(request);
+            return execute.StatusCode;
+        }
+        public HttpStatusCode Set_username(string u, string Cookies)
+        {
+            client = new RestClient(API_BASE);
+            client.Proxy = null;
+            request = new RestRequest(SET_PATH, Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("X-CSRFToken", "missing");
+            headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            headers.Add("Connection", "keep-alive");
+            request.AddCookie("sessionid", Cookies);
+            client.UserAgent = USER_AGENT;
+            client.AddDefaultHeaders(headers);
+            request.AddParameter("", $"username={u}",ParameterType.RequestBody);
+            var execute = client.Execute(request);
+            return execute.StatusCode;
+        }
+        public HttpStatusCode Set_biography(string b, string Cookies)
+        {
+            client = new RestClient(API_BASE);
+            client.Proxy = null;
+            request = new RestRequest(BIO_PATH, Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("X-CSRFToken", "missing");
+            headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            headers.Add("Connection", "keep-alive");
+            request.AddCookie("sessionid", Cookies);
+            client.UserAgent = USER_AGENT;
+            client.AddDefaultHeaders(headers);
+            request.AddParameter("", $"raw_text={b}", ParameterType.RequestBody);
+            var execute = client.Execute(request);
+            return execute.StatusCode;
+        }
+        public HttpStatusCode CHECK_BLOCK(string u, string Cookies)
+        {
+            client = new RestClient(API_BASE);
+            client.Proxy = null;
+            request = new RestRequest(SET_PATH, Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("X-CSRFToken", "missing");
+            headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            headers.Add("Connection", "close");
+            request.AddCookie("sessionid", Cookies);
+            client.UserAgent = USER_AGENT;
+            client.AddDefaultHeaders(headers);
+            request.AddParameter("", $"username={u}", ParameterType.RequestBody);
+            var execute = client.Execute(request);
+            return execute.StatusCode;
+        }
+        public string CURRENT_USER(string Cookies)
+        {
+            client = new RestClient(API_BASE);
+            client.Proxy = null;
+            request = new RestRequest(CURRENT_PATH, Method.GET);
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("X-CSRFToken", "missing");
+            headers.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            headers.Add("Connection", "close");
+            request.AddCookie("sessionid", Cookies);
+            client.UserAgent = USER_AGENT;
+            client.AddDefaultHeaders(headers);
+            var execute = client.Execute(request);
+            username = Regex.Match(execute.Content, "\"username\":\"(.*?)\"").Groups[1].Value  ;
+            email = Regex.Match(execute.Content, "\"email\":\"(.*?)\"").Groups[1].Value;
+            phone_number = Regex.Match(execute.Content, "\"phone_number\":\"(.*?)\"").Groups[1].Value;
+            biography = Regex.Match(execute.Content, "\"biography\":\"(.*?)\"").Groups[1].Value;
+            return username;
+        }
+        public bool CHECK_14(string target)
+        {
+            bool check = false;
+            client = new RestClient(WEB_BASE);
+            client.Proxy = null;
+            request = new RestRequest(CHECK14_PATH, Method.POST);
+            Dictionary<string, string> headers = new Dictionary<string, string>();
+            headers.Add("X-CSRFToken", "missing");
+            headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0";
+            client.AddDefaultHeaders(headers);
+            request.AddParameter("hacker", $"email=&username={target}&first_name=&opt_into_one_tap=false", ParameterType.RequestBody);
+            var execute = client.Execute(request);
+            if (execute.Content.Contains("username_held_by_others"))
+            {
+                check = true;
+            }
+           
+            return check;
+        }
+       
+        public string RandomString(int length)
+        {
+            return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToLower(), length).Select((Func<string, char>)((string s) => s[random.Next(s.Length)])).ToArray());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowManager;
+
+namespace eveSwapper
+{
+    class Program
+    {
+        static public APIs GetAPIs = new APIs();
+        static public string username, target, bio, Session;
+        static public EventWaitHandle waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+        static public EventWaitHandle waitBlocked = new EventWaitHandle(false, EventResetMode.ManualReset);
+        static public EventWaitHandle nothing = new EventWaitHandle(false, EventResetMode.ManualReset);
+        static bool bools;
+        private static HttpStatusCode httpStatus = new HttpStatusCode();
+        public static volatile int BlockCounter;
+        public static volatile int Counter;
+        static public int threads = 5;
+        static public int sleep = 5;
+        static public int loops = 5;
+        static void Main(string[] args)
+        {
+            Console.Title = "eveSwapper";
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("[e] Username : ");
+                try
+                {
+                    Session = File.ReadAllLines($"@{Console.ReadLine()}.txt")[2].Split(':')[1];
+                }
+                catch { }
+                var con = GetAPIs.Consent(Session);
+                if (con == HttpStatusCode.OK)
+                {
+                    username = GetAPIs.CURRENT_USER(Session);
+                   
+                        if (username == null || username.Length < 2)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkRed;
+                            Console.Write("[e] Someting wrong!, try again? [Y/N]: ");
+                            Console.ResetColor();
+                            if (Console.ReadLine().Equals("n", StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                return;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"[e] Logged in {username} Successfully  !");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.Write("[e] Someting wrong!, try again? [Y/N]: ");
+                    Console.ResetColor();
+                    if (Console.ReadLine().Equals("n", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                break;
+            }
+            Console.ResetColor();
+            Console.Write("[e] Target : ");
+            target = Console.ReadLine();
+
+            if (GetAPIs.CHECK_14(target))
+            {
+                Console.WriteLine($"[e] @{target} Can't Swap it ..");
+            }
+            else
+            {
+                Console.WriteLine($"[e] @{target} is swappable");
+            }
+            Console.Write("[e] Bio : ");
+            bio = Console.ReadLine();
+            threads = new Random().Next(25,50);
+            Console.Write("[e] Enter Threads : ");
+            var thre = Console.ReadLine();
+            if (thre != "")
+                threads = int.Parse(thre);
+            loops = new Random().Next(500);
+            Console.Write("[e] Enter Loops : ");
+            var lop = Console.ReadLine();
+            if (lop != "")
+                 loops = int.Parse(lop);
+            Console.Clear();
+            Parallel.For(0, threads, new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount },(integer, LoopState) =>
+            {
+                Thread thread = new Thread(delegate () { RequestSET(); })
+                 {
+                   Priority = ThreadPriority.Highest,
+                   IsBackground = true
+                 };
+                 thread.Start();
+            });
+            Console.Clear();
+            Console.WriteLine($"[e] Target : {target}\n[e] Threads : {threads}\n[e] Loops : {loops}");
+            MsgBoxResult result = Interaction.MsgBox("[e] Press ok when u ready", Console.Title, MsgBoxStyle.Information);
+            waitHandle.Set();
+            Task.Run(() =>
+            {
+                SuperVisior();
+            });
+            nothing.WaitOne();
+        }
+        public static void RequestSET()
+        {
+                waitHandle.WaitOne();
+                while (true)
+                {
+                    if (bools || BlockCounter >= 60) 
+                        return;
+                    for (int i = 0; i < loops; i++)
+                    {
+                        if (bools || BlockCounter >= 60)
+                            return;
+                            httpStatus = GetAPIs.Set_username(target, Session);
+
+                            if (httpStatus == HttpStatusCode.BadRequest)
+                            {
+                                Interlocked.Increment(ref Counter);
+                            }
+                            if (httpStatus == HttpStatusCode.OK)
+                            {
+                                GetAPIs.Set_biography(bio, Session);
+                                Claimed();
+                                Interlocked.Increment(ref Counter);
+                            }
+                            if (httpStatus.ToString() == "429")
+                            {
+                                Interlocked.Increment(ref BlockCounter);
+                            }
+                    }
+                   sleep = new Random().Next(150);
+                   if (sleep == 5)
+                      sleep = 1;
+                   Thread.Sleep(TimeSpan.FromMilliseconds(sleep));
+                } 
+        }
+        public static void Claimed()
+        {
+            bools = true;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[e] Successfully Swapped @{target}");
+            Console.ResetColor();
+            Console.WriteLine($"[e] Attempts: {Counter} , Spam: {BlockCounter}");
+            Environment.Exit(0);
+        }
+        public static void SuperVisior()
+        {
+            while (true)
+                if (!bools)
+                    Console.Write($"[e] Attempts: {Counter} , Spam: {BlockCounter}\r");
+            
+        }
+    }
+}
